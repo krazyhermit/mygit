b1d9bf75218822ad8fbc39a10be1e8d6b13f0b66
Mohammad N Haq <myemail@myemail.com>
2017-05-20 04:14:44 -0400

    Commit-1

---
+++ maven.project.one/src/maven/project/one/one/HelloMaven.java
@@ -0,0 +1,14 @@
+package maven.project.one.one;
+
+public class HelloMaven {
+	
+	public HelloMaven(){
+		System.out.println("Hello there you have invoked the default constructor!");
+	}
+	public int sum(int a,int b){
+		int c = a+b;
+		return c;
+	}
+	
+	
+}
