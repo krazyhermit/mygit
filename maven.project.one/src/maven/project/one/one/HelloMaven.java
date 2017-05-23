ffc2a9691bcf8a3af47c8e7489fa89c0b0407281
Mohammad N Haq <myemail@myemail.com>
2017-05-23 01:10:33 -0400

    added few new comment

--- maven.project.one/src/maven/project/one/one/HelloMaven.java
+++ maven.project.one/src/maven/project/one/one/HelloMaven.java
@@ -5,10 +5,14 @@ public class HelloMaven {
 	public HelloMaven(){
 		System.out.println("Hello there you have invoked the default constructor!");
 	}
+	
+	//adding method
 	public int sum(int a,int b){
 		int c = a+b;
 		return c;
 	}
+	
+	//printing method
 	public void print(){
 		System.out.println("Test Printing");
 	}
